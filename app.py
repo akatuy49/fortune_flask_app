21abe872308d6e738dad389e2b17b620deabcbfd
akatuy49 <c35_194@yahoo.co.jp>
2025-06-17 20:09:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -93,5 +93,6 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    if __name__ == "__main__":
+     app.run()
+
